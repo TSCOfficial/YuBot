fd161d4be64a2834a847acd2773599496fa5cbec
Aaron Frily <afr149360@stud.gibb.ch>
2026-06-11 14:00:11 +0200

    added rules container

---
+++ src/main/java/ch/frily/yubot/container/RulesContainer.java
@@ -0,0 +1,11 @@
+package ch.frily.yubot.container;
+
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+
+public class RulesContainer extends Container {
+
+    public RulesContainer() {
+        this.addComponent(TextDisplay.of("## Regelwerk"));
+        this.addComponent(TextDisplay.of("-# *Vom 16. Januar 2026 - Stand 11. Juni 2026*"))
+    }
+}
