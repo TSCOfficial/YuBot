c4b868507a0405f62499a4189aa80ecdaf5db719
Aaron Frily <afr149360@stud.gibb.ch>
2026-08-12 17:10:55 +0200

    Modified rule container

--- src/main/java/ch/frily/yubot/container/RulesContainer.java
+++ src/main/java/ch/frily/yubot/container/RulesContainer.java
@@ -22,7 +22,7 @@ public class RulesContainer extends Container {
         this.addComponent(MediaGallery.of(MediaGalleryItem.fromFile(
                 FileUpload.fromData(getClass().getResourceAsStream("/img/regelwerk-header.png"), "regelwerk-header.png"))));
         this.addTextDisplay(String.format("## Serverregeln von %s", GUILD.getName()));
-        this.addTextDisplay("-# *Vom 15. Juni 2026 (Stand 02. August 2026)*");
+        this.addTextDisplay("-# *Vom 15. Juni 2026 (Stand 11. August 2026)*");
 
         this.addTextDisplay(String.format("Dies sind die allgemein gültigen Regeln des öffentlichen Community-Servers: \"%s\". Sie sind entsprechend des Reglements verbindlich für alle Mitglieder des Discord-Servers.", GUILD.getName()));
         this.addInvisibleSeparator(Separator.Spacing.LARGE);
@@ -36,6 +36,7 @@ public class RulesContainer extends Container {
                 - §5 Voicechats
                 - §6 Spam
                 - §6a Bewerbung anderer Discord Server
+                - §6b Gruppierungen und Zugehörigkeitsmerkmale
                 - §7 Umgang mit Regelbrüchen
                 - §8 Server-Staff
                 - §9 Vergabe und Entzug erweiterter Rechte
@@ -43,146 +44,9 @@ public class RulesContainer extends Container {
                 - §11 Zusäzliche Werke
                 """);
 
-        this.buildContainerWithCurrentComponents();
-
-        this.addTextDisplay("**§1 Zielsetzung**");
-        this.addTextDisplay(String.format("""
-                       %s ist der offizielle deutschsprachige Community-Server von Yu. Auf ihm soll ein gemütliches, friedliches und freundschaftliches Miteinander ermöglicht werden.
-                        """, GUILD.getName())
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§2 Rechte und Pflichten einer Mitgliedschaft**");
-        this.addTextDisplay("¹ Das Beitreten und Verweilen auf dem Servers steht jeder Person frei zur Wahl, welche 16 oder Älter ist.");
-        this.addTextDisplay("""
-                ² Benutzername und Nicknames dürfen §3 nicht verletzen. Nicknames dienen der alltäglichen Identifizierung von Anderen und sollten deswegen möglichst einzigartig sein. Entspricht ein Nickname nicht den genannten Regeln, wird er vom Serverteam geändert.
-                """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§3 Recht des Anderen**");
-        this.addTextDisplay("""
-                ¹ Auf diesem Server hat jede Person das Recht, würdevoll und respektvoll behandelt zu werden. Belästigung, Sexismus, Rassismus, Volksverhetzung, Queerfeindlichkeit und Diskriminierung werden nicht toleriert.
-                ² Das Immitieren von Personen auf diesem Server verstößt gegen das Recht des Anderen und wird dementsprechend behandelt.
-                ³ Das Verbreiten von Falschaussagen oder gefälschten Nachrichten fällt ebenfalls unter würde- und respektloses Verhalten.
-                ⁴ Das versenden von Inhalten mit Stroboskopischen, oder sich schnell bewegenden, Inhalten wird nicht toleriert.
-                Die Konsequenzen, die durch einen Verstoß dieses Paragraphen in Kraft treten, können unter §7 nachgelesen werden
-                        """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§4 Inhalte für Erwachsene**");
-        this.addTextDisplay("""
-                        NSFW- oder obszöne Inhalte sind nicht erwünscht. Dazu zählen Texte, Bilder oder Links mit Nacktheit, Sex, schwerer Gewalt oder anderen grafisch verstörenden Inhalten.
-                        """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§5 Voicechats**");
-        this.addTextDisplay("""
-                        ¹ Jede Person hat die Pflicht, sich in Voicechats respektvoll und maßvoll zu verhalten und Andere nicht zu stören. In Voicechats ist es untersagt, jegliche Formen von Drogen (auch Alkohol, Vapes oder Zigaretten) zu konsumieren.
-                        ² Auch Regelverstöße in Sprachchats führen zu Konsequenzen. Moderator\\*innen und Supporter\\*innen können in den Voicechats nach dem Rechten und dem Wohlbefinden schauen.
-                        """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§6 Spam**");
-        this.addTextDisplay("""
-                        Spam ist nicht erwünscht. Formen von Spam sind:
-                        1. Missbrauch von Pings
-                        2. Missbrauch von Kanälen/Kanalfunktionen
-                        3. Übermäßiges, hochfrequentiertes Schreiben von Nachrichten
-                        4. NSFW Inhalte jeglicher Art
-                        5. Scamversuche
-                        6. nicht authorisierte Fremd- & Eigenwerbung
-                        7. Belästigung anderer Servermitglieder a) auf dem Server b) in den DMs
-                        8. Regelwidrige Nicknames
-                        
-                        Regelbrüche werden nach §7 geahndet.
-                        """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§6a Bewerbung anderer Discord Server**");
-        this.addTextDisplay("""
-                        Wer andere Mitglieder über diesen Server dazu bewegt, auf andere Discord Server zu joinen, wird zum Schutz von Minderjährigen gebannt, da diese Server nicht von uns moderiert werden und wir nicht gewährleisten können, dass diese auch sicher sind.
-                        """
-        );
-
-        this.buildContainerWithCurrentComponents();
-
-        this.addTextDisplay("**§7 Umgang mit Regelbrüchen**");
-        this.addTextDisplay("""
-                        ¹ *Von regulären Mitgliedern*
-                        Bei Regelbrüchen entscheiden die Moderatoren, ob ein Regelbruch vorliegt und ob bzw. welche Strafe angemessen ist. Mögliche Strafen sind u.a.:
-                        - Timeout
-                        - Kick
-                        - Bann
-                        """
-        );
-        this.addTextDisplay("""
-                ² *Von Server-Staff*
-                Ein Mitglied des Server-Staffs ist zu entlassen, wenn die Person
-                a. unbegründete Strafen verhängt
-                b. gegen §3, §4, §6 oder §9 verstößt
-                c. nach eigennützigen Motiven als Server-Staff agiert und damit anderen schadet
-                d. Persönliche Bekannte bevorzugt
-                """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§8 Server-Staff**");
-        this.addTextDisplay("""
-                Mitglieder des Server-Staff sind verpflichtet, sich den Serverregeln entsprechend zu verhalten und als Vorbild vorrauszugehen. Sie haben im Zweifelsfall immer das letzte Wort, bis ein Mitglied höheren Ranges etwas anderes sagt.
-           
-                Jedes der unten genannten Teams verfügt über eine oder mehrere Leitungspersonen, die mittels eigener Rolle besonders gekennzeichnet sind. Diese verteilen die Aufgaben im Team und sind für ihre Teammitglieder verantwortlich.
-                - SocialMedia-Team : Organisiert und Produziert Inhalte für Social Media.
-                - Event-Team : Organisiert die Events auf dem Server.
-                - Awareness-Team : Hilfe bei persönlichen Problemen und Konflikten.
-                - Support\\*innen : Sorgen für Einhaltung der Regeln, beantworten Tickets. Können timeouten.
-                - Moderator\\*innen : Wie Supporter\\*innen, aber entscheiden und verhängen Strafen. Können zusätzlich kicken und bannen.
-                - Development-Team : Kümmern sich um alles technische an Server und Bots.
-                - Serverleitung : Setzt den Server im Auftrag von Yu um.
-                - Owner : Besitzt den Servers und die Ownerrechte, höchste Instanz.
-                """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§9 Vergabe und Entzug erweiterter Rechte**");
-        this.addTextDisplay("""
-                ¹ Die Vergabe von erweiterten Rechten erfolgt in der Regel direkt durch den Vorgesetzten und immer in der Annahme, dass mit ihnen verantwortungsvoll und im Sinne des Servers umgegangen wird.
-                ² Ist die verantwortungsvolle Benutzung der Rechte aus Sicht des Serverowners, der Serverleitung oder der zuständigen Teamleitung nicht gegeben, können diese Rechte jederzeit wieder entzogen werden.
-                """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§10 Datenschutzgrundverordnung (DSGVO)**");
-        this.addTextDisplay("""
-                Der Schutz der Privatsphäre und persönlichen Daten aller User*innen ist uns sehr wichtig. Auf diesem Server gilt somit ausdrücklich die DSGVO.
-                Dies bedeutet z.B., dass das Aufnehmen von anderen Personen in Voice Calls, das Fotografieren, Veröffentlichen von Screenshots, Weitergeben privater Inhalte oder Verarbeiten personenbezogener Daten ist ohne ausdrückliche Zustimmung der betroffenen Person nicht gestattet ist.
-                Dies gilt insbesondere für Voice Calls, private Nachrichten, Bilder, persönliche Daten und andere sensible Inhalte. Verstöße gegen diese Regel können je nach Schwere Konsequenzen bis hin zum Ausschluss vom Server haben.
-                """
-        );
-
-        this.addInvisibleSeparator(Separator.Spacing.SMALL);
-
-        this.addTextDisplay("**§11 Zusäzliche Werke**");
-        this.addTextDisplay("Zusätzlich zu allen Server Regeln gelten auf diesem Server die deutschen Gesetze (Grundgesetz, etc.), sowie die Discord Nutzungsbedingungen (ToS).");
-        this.addComponent(
-                ActionRow.of(
-                        Button.link("https://www.gesetze-im-internet.de/", "Deutsches Gesetz"),
-                        Button.link("https://discord.com/terms", "Discord ToS")
-                )
-        );
+        this.addSection(Button.link("https://doc.einfachyu.de/share/3od59b6uhi/p/serverregeln-hhcK6YDHIr", "Serverregeln"),
+                TextDisplay.of("**Sieh dir unsere Serverregeln direkt auf unserem DocMost an.**")
+                );
 
         this.addInvisibleSeparator(Separator.Spacing.SMALL);
 
