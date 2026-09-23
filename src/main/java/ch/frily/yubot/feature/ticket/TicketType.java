6d87d59bb7761824209060f5a3b0d5e036ff703e
Aaron Frily <thestreakcraft.official@gmail.com>
2026-09-23 21:22:20 +0200

    added design application ticket

--- src/main/java/ch/frily/yubot/feature/ticket/TicketType.java
+++ src/main/java/ch/frily/yubot/feature/ticket/TicketType.java
@@ -182,12 +182,21 @@ public enum TicketType {
             TicketTypeGroup.BEWERBUNG,
             List.of(EnvKey.ROLE_EVENTLEITUNG)
     ),
-    BEWERBUNG_SOCIALMEDIA(
-            "besm",
-            "🎬 Fachbereich Social Media",
-            "Bewerbung Fachbereich Social Media",
+    BEWERBUNG_DESIGN(
+            "bedes",
+            "🎨 Fachbereich Design",
+            "Bewerbung Fachbereich Design (16+)",
             """
-                    *Dieser Text ist noch nicht vorhanden*
+                    Bitte verfasse deine Bewerbung. Für deine Bewerbung ist uns Folgendes wichtig::
+                    
+                    - Kurze Vorstellung (Name, Alter, Pronomen, ...)
+                    - Warum möchtest du Teil des Designteams werden?
+                    - Deine zeitliche Verfügbarkeit
+                    - Bisherige Erfahrungen in dem Bereich
+                    - 2-3 digitale Zeichnungen/Designs, die uns Einblick in euer Stil geben
+                    Du kannst gerne mehr schreiben!
+                    
+                    Sobald du deine Bewerbung geschrieben hast, meldet sich die zuständige Person bei dir.
                     """,
             TicketTypeGroup.BEWERBUNG,
             List.of(EnvKey.ROLE_SERVERLEITUNG)
